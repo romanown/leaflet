088eba6fd4898e9d8641c8576c4aa53ea7167c93
romanown <qwerty012345@yandex.ru>
2022-08-12 17:01:33 +0300

    added onClick to select

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,21 +1,26 @@
 import type { FC } from 'react';
+import { useState } from 'react';
 
 import styles from './Row.module.css';
 
 import SelectItem from 'components/Select';
 import type { rowDataType } from 'types/lists';
 
-const Row: FC<rowDataType> = ({ name, start, end }: rowDataType) => (
-  <div className={styles.main}>
-    <div className={styles.row}>
-      <div>{name}</div>
-      <div>
-        <SelectItem item={start} />
-      </div>
-      <div>
-        <SelectItem item={end} />
+const Row: FC<rowDataType> = ({ name, start, end }: rowDataType) => {
+  const [currentPosStart, setCurrentPosStart] = useState('');
+  const [currentPosEnd, setCurrentPosEnd] = useState('');
+  return (
+    <div className={styles.main}>
+      <div className={styles.row}>
+        <div>{name}</div>
+        <div>
+          <SelectItem item={start} currentPos={currentPosStart} onPosChange={setCurrentPosStart} />
+        </div>
+        <div>
+          <SelectItem item={end} currentPos={currentPosEnd} onPosChange={setCurrentPosEnd} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default Row;
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -10,39 +10,44 @@ const { Option } = Select;
 
 type itemType = {
   item: string;
+  currentPos: string;
+  onPosChange: Function;
 };
 
-const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
-};
+const SelectItem: FC<itemType> = ({ item, currentPos, onPosChange }: itemType) => {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+    onPosChange(value);
+  };
 
-const onSearch = (value: string) => {
-  console.log(`selected ${value}`);
-};
+  const onSearch = (value: string) => {
+    console.log(`selected ${value}`);
+  };
 
-const SelectItem: FC<itemType> = ({ item }: itemType) => (
-  <div className={styles.main}>
-    <div>{item}</div>
-    <div>
-      <Select
-        showSearch
-        placeholder="Select a person"
-        optionFilterProp="children"
-        onSearch={onSearch}
-        filterOption={(input, option) =>
-          (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
-        }
-        defaultValue="lucy"
-        style={{ width: 120 }}
-        onChange={handleChange}
-      >
-        {listCity.map((city) => (
-          <Option key={city.name} value={city.name}>
-            {city.name}
-          </Option>
-        ))}
-      </Select>
+  return (
+    <div className={styles.main}>
+      <div>{item}</div>
+      <div>
+        <Select
+          showSearch
+          placeholder="Select a person"
+          optionFilterProp="children"
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
+          }
+          defaultValue={currentPos}
+          style={{ width: 120 }}
+          onChange={handleChange}
+        >
+          {listCity.map((city) => (
+            <Option key={city.name} value={city.name}>
+              {city.name}
+            </Option>
+          ))}
+        </Select>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default SelectItem;
